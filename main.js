c62879871908d7750edaaa8fd1b9ca8db78f67ef
Semir Selman <selmansemir555@gmail.com>
2022-09-16 14:18:06 +0200

    made the navigation responsible

--- main.js
+++ main.js
@@ -6,7 +6,8 @@ btn_nav.forEach(element => {
     element.addEventListener('click', (event) => {
 
         const li = event.target.closest('li').querySelector('.list');
-        const img = event.target.closest('.btn-nav').querySelector('img');
+        const arrow_light = event.target.closest('.btn-nav').querySelector('.arrow-light');
+        const arrow_dark = event.target.closest('.btn-nav').querySelector('.arrow-dark');
         const label = event.target.closest('.btn-nav').querySelector('label');
 
         btn_nav.forEach(el => {
@@ -17,8 +18,16 @@ btn_nav.forEach(element => {
 
             }
 
-            if( (alEl != li) && (el.querySelector('img').classList.contains('rotation') && el.querySelector('label').classList.contains('highlight'))){
-                el.querySelector('img').classList.remove('rotation')
+            if( (alEl != li) &&
+            (
+            (el.querySelector('.arrow-light').classList.contains('rotation') || el.querySelector('.arrow-dark').classList.contains('rotation'))
+            &&
+            el.querySelector('label').classList.contains('highlight')
+            )
+            )
+            {
+                el.querySelector('.arrow-light').classList.remove('rotation');
+                el.querySelector('.arrow-dark').classList.remove('rotation')
                 el.querySelector('label').classList.remove('highlight');
             }
 
@@ -27,7 +36,36 @@ btn_nav.forEach(element => {
         li.classList.toggle('show');
 
         label.classList.toggle('highlight');
-        img.classList.toggle('rotation');
+        // label.style.color = 'red';
+        arrow_light.classList.toggle('rotation');
+        arrow_dark.classList.toggle('rotation');
+
+
     });
 
+});
+
+const hamb_menu = document.querySelector('#haburger-img');
+const close_menu = document.querySelector('#close-img');
+
+const nav_container = document.querySelector('#nav-container');
+
+hamb_menu.addEventListener('click', (e) => {
+
+    e.target.style.display = 'none';
+    close_menu.style.display = 'block';
+
+    nav_container.classList.toggle('nav-show');
+
+    console.log('open');
+    
+});
+
+close_menu.addEventListener('click', (e) => {
+
+    e.target.style.display = 'none';
+    hamb_menu.style.display = 'block';
+
+    nav_container.classList.toggle('nav-show');
+    console.log('close');
 });
